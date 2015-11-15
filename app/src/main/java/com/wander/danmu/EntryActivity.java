fa39f3ed3a7deab65e24cb1e2200c1168fb993c0
wander0515 <805677461@qq.com>
2015-11-16 00:25:04 +0800

    change

--- app/src/main/java/com/wander/danmu/EntryActivity.java
+++ app/src/main/java/com/wander/danmu/EntryActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class EntryActivity extends AppCompatActivity {
     public static Rect rect;
 
@@ -13,6 +16,13 @@ public class EntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(EntryActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 100);
 
     }
 
@@ -20,7 +30,5 @@ public class EntryActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         rect = PixelTools.getStatusHeight(this);
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
     }
 }
